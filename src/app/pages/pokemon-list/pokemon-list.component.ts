3d0580ee8d8b368c47549670c764b0705b02b910
georgedavid1 <georgedavid1@gmail.com>
2020-05-03 16:23:15 -0700

    was able to get pokemon list with nested subcribe

--- src/app/pages/pokemon-list/pokemon-list.component.ts
+++ src/app/pages/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { mergeMap, map } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+export class Data {
+  results:any[];
+}
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
@@ -10,18 +14,34 @@ export class PokemonListComponent {
 
   title: string = 'pokemon-dictionary';
   pokemon: any;
+  pokemonList = [];
   sales: any[];
   headers: any[];
+  test: any;
   constructor(
     private httpClient: HttpClient
   ) {
-    this.httpClient.get('https://pokeapi.co/api/v2/pokemon').subscribe((data: any)=>{
-      this.pokemon = data.results;
-    })
+    //1) nested subsrbe. =
+    //2) after that works try merging
+    this.httpClient.get('https://pokeapi.co/api/v2/pokemon')
+    .subscribe((data: any)=>{
+      forkJoin
+        data.results.forEach((element:any) => {
+            this.httpClient.get(element.url).subscribe((data:any)=>{
+              this.pokemonList.push(data);
+            });
+        });
+    });
+
     this.headers = [
+      { field: 'id', header: 'ID' },
       { field: 'name', header: 'Name' },
-      { field: 'url', header: 'URL' }
+      { field: 'photo', header: 'Photo' }
     ];
   }
 
 }
+// .results.forEach(element => {
+//   // console.log('ELEMENT ' + JSON.stringify(element));
+//   this.httpClient.get(element.url)
+// }))
